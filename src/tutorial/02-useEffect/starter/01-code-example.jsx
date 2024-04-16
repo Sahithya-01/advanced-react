e636b4d1e247e732f094fa231d46874bbc3f7bc4
“Sahithya <sahithya.hanumandla01@gmail.com>
2024-04-16 17:48:34 -0400

    useEffect except cleanUp function is done

--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const CodeExample = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0)
+  const sayHi = () => {
+    console.log('Hi there!!')
+    setValue(value + 1)
+  }
+
+  sayHello() // we will have an infinite loop
 
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
-  );
-};
-export default CodeExample;
+  )
+}
+export default CodeExample
--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,56 @@
-const url = 'https://api.github.com/users';
+import { useState, useEffect } from 'react'
+
+const url = 'https://api.github.com/users'
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
-};
-export default FetchData;
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    // fetch(url)
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       throw new Error(response.statusText)
+    //     }
+    //     return response.json()
+    //   })
+    //   .then((data) => {
+    //     setUsers(data)
+    //     console.log(data)
+    //   })
+    //   .catch((error) => {
+    //     console.error(error)
+    //   })
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+        const users = await response.json()
+        setUsers(users)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }),
+    []
+
+  return (
+    <section>
+      <h3>github users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              {/* const {id,login,avatar_url,html_url} */}
+              <img src={user.avatar_url} alt={user.login} />
+              <div>
+                <h5>{user.login}</h5>
+                <a href={user.html_url}>Profile</a>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </section>
+  )
+}
+export default FetchData
