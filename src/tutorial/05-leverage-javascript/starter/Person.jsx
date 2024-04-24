05d22fe3d75f86d6180569fcf8852be1dfdcac9b
“Sahithya <sahithya.hanumandla01@gmail.com>
2024-04-24 11:14:57 -0400

    corrected image tag

--- src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -5,7 +5,7 @@ export function Person({ name, nickName = 'default value', images }) {
   //Where the logical or (|| ) operator takes the right operand in the case of a falsy value — which includes empty string, 0, false, NaN, etc. The nullish coalescing operator (?? ) takes the right operand only when the left side is set to null or undefined .
   return (
     <div>
-      <img src={img} alt={name} style={{ width: '75px' }}></img>
+      <img src={img} alt={name} style={{ width: '75px' }} />
       <h4>{name}</h4>
       <p>Nickname: {nickName}</p>
     </div>
